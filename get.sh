c94703c0601776ed6d98db82a9d4e9cd2267e707
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-12-05 17:14:47 +0900

    Sleep when some error happens

--- get.sh
+++ get.sh
@@ -2,6 +2,17 @@
 
 while :
 do
+  date
+  echo "--- Start Call API"
   python netatmo2influxdb.py
+  RET=$?
+  if [ ${RET} -ne 0 ];
+  then
+    echo "Exit status not 0"
+    echo "Sleep 120"
+    sleep 120
+  fi
+  date
+  echo "Sleep 60"
   sleep 60
 done
